e139078a7a6c579b99ec9ddb479a84409ff631f7
Rickard Lindahl <mail@rickardlindahl.com>
2022-10-06 17:15:49 +0200

    feat(ekko): add zod env validation

---
+++ services/ekko/src/env/schema.ts
@@ -0,0 +1,16 @@
+import { z, type ZodFormattedError } from "zod";
+
+export const serverSchema = z.object({
+  PORT: z.number().default(3000),
+  NODE_ENV: z.enum(["development", "test", "production"]),
+});
+
+export const formatErrors = (
+  errors: ZodFormattedError<Map<string, string>, string>
+) =>
+  Object.entries(errors)
+    .map(([name, value]) => {
+      if (value && "_errors" in value)
+        return `${name}: ${value._errors.join(", ")}\n`;
+    })
+    .filter(Boolean);
--- services/ekko/src/server.ts
+++ services/ekko/src/server.ts
@@ -1,7 +1,8 @@
 import express, { Request, Response } from "express";
+import { env } from "./env/server";
 
 const app = express();
-const port = 3000;
+const port = env.PORT;
 
 app.get("/", (_req: Request, res: Response) => {
   res.send("Express + TypeScript Server");
