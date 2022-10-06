11093d45b79d8be0fcd2c68415f47f4a006150ed
Rickard Lindahl <mail@rickardlindahl.com>
2022-10-06 17:24:10 +0200

    fix(config): update tailwind content paths

--- apps/storybook/tailwind.config.js
+++ apps/storybook/tailwind.config.js
@@ -1,8 +1,2 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./src/**/*.{js,ts,tsx,jsx}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
+module.exports = require("config/tailwind.config");
--- packages/config/tailwind.config.js
+++ packages/config/tailwind.config.js
@@ -1,9 +1,8 @@
 module.exports = {
   content: [
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./stories/**/*.{js,ts,jsx,tsx}",
     "../../packages/ui/**/*.{js,ts,jsx,tsx}",
-    "../../packages/teemo/src/**/*.{js,ts,jsx,tsx}",
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
