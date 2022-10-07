121699f757291669a88249f99a5ff1e6af79ef56
Viktor Malmedal <viktor.malmedal@eniro.com>
2022-10-07 17:52:46 +0200

    feat(ui): add iconbutton

---
+++ packages/ui/src/components/IconButton.tsx
@@ -0,0 +1,70 @@
+import * as React from "react";
+import { clsx } from "clsx";
+
+type IconButtonColors =
+  | "primary"
+  | "secondary"
+  | "accent"
+  | "info"
+  | "success"
+  | "warning"
+  | "error";
+
+type IconButtonVariants = "square" | "circle";
+
+type IconButtonSizes = "lg" | "md" | "sm" | "xs";
+
+export type IconButtonProps = Omit<
+  React.DetailedHTMLProps<
+    React.ButtonHTMLAttributes<HTMLButtonElement>,
+    HTMLButtonElement
+  >,
+  "color" | "children"
+> & {
+  children?: React.ReactNode;
+  color?: IconButtonColors;
+  variant?: IconButtonVariants;
+  size?: IconButtonSizes;
+  disableAnimation?: boolean;
+  active?: boolean;
+  disabled?: boolean;
+  loading?: boolean;
+  wide?: boolean;
+};
+
+const sizes = {
+  lg: "h-10 w-10",
+  md: "h-8 w-8",
+  sm: "h-6 w-6",
+  xs: "h-4 w-4",
+};
+
+export const IconButton = ({
+  color,
+  children,
+  loading,
+  wide,
+  disabled,
+  active,
+  className,
+  size = "md",
+  variant = "square",
+  ...props
+}: IconButtonProps) => (
+  <button
+    className={clsx(
+      "btn",
+      { [`btn-${size}`]: !!size },
+      { [`btn-${variant}`]: !!variant },
+      { [`btn-${color}`]: !!color },
+      { loading: !!loading },
+      { [`btn-${wide}`]: !!wide },
+      { [`btn-${disabled}`]: !!disabled },
+      { [`btn-${active}`]: !!active },
+      className
+    )}
+    {...props}
+  >
+    {!loading && <div className={sizes[size]}>{children}</div>}
+  </button>
+);
