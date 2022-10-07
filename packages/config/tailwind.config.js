b4999f8d521ab588cfcb90c9b176732601115516
Viktor Malmedal <viktor.malmedal@eniro.com>
2022-10-07 17:51:56 +0200

    feat(config): update daisyui

--- packages/config/tailwind.config.js
+++ packages/config/tailwind.config.js
@@ -1,4 +1,21 @@
 module.exports = {
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#33A9FE",
+          secondary: "#d8b4fe",
+          accent: "#C149AD",
+          neutral: "#172231",
+          "base-100": "#121E2A",
+          info: "#1e40af",
+          success: "#1EB972",
+          warning: "#F3AF2D",
+          error: "#E94E30",
+        },
+      },
+    ],
+  },
   content: [
     "./src/**/*.{js,ts,jsx,tsx}",
     "./stories/**/*.{js,ts,jsx,tsx}",
