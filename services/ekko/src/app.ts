820e5f1d87f18d2ce493a6ddfb0837761ae18e49
Rickard Lindahl <mail@rickardlindahl.com>
2022-10-07 08:57:28 +0200

    feat(ekko): add api router

--- services/ekko/src/app.ts
+++ services/ekko/src/app.ts
@@ -1,4 +1,5 @@
 import express, { Express, Request, Response } from "express";
+import { apiRouter } from "./routes/api";
 import { healthcheck } from "./routes/healthcheck";
 
 export const createApp = (): Express => {
@@ -6,5 +7,6 @@ export const createApp = (): Express => {
 
   app.get("/healthcheck", healthcheck);
 
+  app.use("/api", apiRouter);
   return app;
 };
