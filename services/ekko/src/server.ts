6bdd6c8ddd655ad05091a87d1bceb61c72b7039b
Rickard Lindahl <mail@rickardlindahl.com>
2022-10-07 08:05:24 +0200

    feat(ekko): add gracefulShutdown

--- services/ekko/src/server.ts
+++ services/ekko/src/server.ts
@@ -1,12 +1,15 @@
 import { createApp } from "./api";
 import { env } from "./env/server";
+import { gracefulShutdown } from "./gracefulShutdown";
 
 const port = env.PORT;
 
 const app = createApp();
 
 if (require.main === module) {
-  app.listen(port, () => {
+  const server = app.listen(port, () => {
     console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
   });
+
+  gracefulShutdown(server);
 }
