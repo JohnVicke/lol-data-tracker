82ad97beb1a0a8c929fada6e2da6404e29db5008
Viktor Malmedal <viktor.malmedal@eniro.com>
2022-10-10 12:10:11 +0200

    test

--- apps/storybook/stories/components/Button.stories.tsx
+++ apps/storybook/stories/components/Button.stories.tsx
@@ -5,7 +5,7 @@ import { createStory } from "../../utils/createStory";
 import { Button } from "ui";
 import type { ButtonProps } from "ui";
 
-console.log("hello worl");
+console.log("hello world");
 
 export const Base: ComponentStory<typeof Button> = (args: ButtonProps) => <Button {...args}>button</Button>;
 
