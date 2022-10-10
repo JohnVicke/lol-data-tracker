944aebd07f18ffeec9efd3014f1945edd9e28bf3
Viktor Malmedal <viktor.malmedal@eniro.com>
2022-10-10 12:23:32 +0200

    feat

--- apps/storybook/stories/components/Button.stories.tsx
+++ apps/storybook/stories/components/Button.stories.tsx
@@ -5,7 +5,7 @@ import { createStory } from "../../utils/createStory";
 import { Button } from "ui";
 import type { ButtonProps } from "ui";
 
-console.log("lsdfol");
+console.log("lsdasdfjasdffol");
 
 export const Base: ComponentStory<typeof Button> = (args: ButtonProps) => <Button {...args}>button</Button>;
 
