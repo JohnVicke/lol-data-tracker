42f26a5719e49d83d868432e1b7d8e5930b7d8f9
Viktor Malmedal <viktor.malmedal@eniro.com>
2022-10-10 12:59:34 +0200

    boomer

--- services/ekko/src/app.ts
+++ services/ekko/src/app.ts
@@ -5,6 +5,7 @@ import { healthcheck } from "./routes/healthcheck";
 export const createApp = (): Express => {
   const app = express();
 
+  console.log("hello world");
   app.get("/healthcheck", healthcheck);
 
   app.use("/api", apiRouter);
