207154b723efba1c0c04434dfb14c28f975268c5
Viktor Malmedal <viktor.malmedal@eniro.com>
2022-10-18 14:01:37 +0200

    fix(storybook): no variants for footer

--- apps/storybook/stories/components/Footer.stories.tsx
+++ apps/storybook/stories/components/Footer.stories.tsx
@@ -6,10 +6,7 @@ import { Footer } from "ui";
 import type { FooterProps } from "ui";
 import { BeakerIcon } from "@heroicons/react/24/solid";
 
-export const Base: ComponentStory<typeof Footer> = (args: FooterProps) => <Footer {...args} />;
-
-export const Primary = Base.bind({});
-Primary.args = {
+const props = {
   logo: <div>Logo</div>,
   socialLinks: [
     {
@@ -90,5 +87,7 @@ Primary.args = {
   copyRight:
     "Copyright © 2022- imbalol. imbalol isn't endorsed by Riot Games and doesn't reflect the views or opinions of Riot Games or anyone officially involved in producing or managing Riot Games properties. Riot Games, and all associated properties are trademarks or registered trademarks of Riot Games, Inc.",
 };
+
+export const Base: ComponentStory<typeof Footer> = (args: FooterProps) => <Footer {...args} {...props} />;
 
 export default createStory(Footer);
