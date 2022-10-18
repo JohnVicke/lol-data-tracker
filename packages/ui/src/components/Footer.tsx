3f530dfdbae5cd7536f37679ebe0e4657a498c97
Viktor Malmedal <viktor.malmedal@eniro.com>
2022-10-18 13:56:30 +0200

    feat(ui): add footer

---
+++ packages/ui/src/components/Footer.tsx
@@ -0,0 +1,52 @@
+import React from "react";
+import { IconButton } from "./IconButton";
+
+type FooterRowLink = {
+  text: string;
+  href: string;
+  icon?: React.ReactNode;
+};
+
+type FooterRow = {
+  title: string;
+  links: FooterRowLink[];
+};
+
+export type FooterProps = {
+  logo: React.ReactNode;
+  copyRight: string;
+  socialLinks?: FooterRowLink[];
+  rows: FooterRow[];
+};
+
+export const Footer = ({ rows, logo, copyRight, socialLinks }: FooterProps) => {
+  return (
+    <footer className="flex flex-col p-10 bg-neutral">
+      <div className="flex flex-wrap gap-6 justify-between items-center">
+        {rows.map(row => (
+          <div key={row.title}>
+            <h3 className="footer-title whitespace-nowrap">{row.title}</h3>
+            <ul className="flex flex-col gap-2">
+              {row.links.map(link => (
+                <li key={link.href}>
+                  <a className="link link-hover whitespace-nowrap" href={link.href}>
+                    {link.text}
+                  </a>
+                </li>
+              ))}
+            </ul>
+          </div>
+        ))}
+      </div>
+      <div className="divider my-4" />
+      {copyRight}
+      <div className="ml-auto">
+        <ul className="flex gap-4 ">
+          {socialLinks?.map(({ icon, href, text }) => (
+            <IconButton key={href}>{icon}</IconButton>
+          ))}
+        </ul>
+      </div>
+    </footer>
+  );
+};
