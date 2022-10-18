afd4ee6b1f6f1e9c78dfbe18936171b097ac41af
Viktor Malmedal <viktor.malmedal@eniro.com>
2022-10-18 14:05:55 +0200

    fix(storybook): add margin to footer social

--- packages/ui/src/components/Footer.tsx
+++ packages/ui/src/components/Footer.tsx
@@ -19,34 +19,32 @@ export type FooterProps = {
   rows: FooterRow[];
 };
 
-export const Footer = ({ rows, logo, copyRight, socialLinks }: FooterProps) => {
-  return (
-    <footer className="flex flex-col p-10 bg-neutral">
-      <div className="flex flex-wrap gap-6 justify-between items-center">
-        {rows.map(row => (
-          <div key={row.title}>
-            <h3 className="footer-title whitespace-nowrap">{row.title}</h3>
-            <ul className="flex flex-col gap-2">
-              {row.links.map(link => (
-                <li key={link.href}>
-                  <a className="link link-hover whitespace-nowrap" href={link.href}>
-                    {link.text}
-                  </a>
-                </li>
-              ))}
-            </ul>
-          </div>
+export const Footer = ({ rows, logo, copyRight, socialLinks }: FooterProps) => (
+  <footer className="flex flex-col p-10 bg-neutral">
+    <div className="flex flex-wrap gap-6 justify-between items-center">
+      {rows.map(row => (
+        <div key={row.title}>
+          <h3 className="footer-title whitespace-nowrap">{row.title}</h3>
+          <ul className="flex flex-col gap-2">
+            {row.links.map(link => (
+              <li key={link.href}>
+                <a className="link link-hover whitespace-nowrap" href={link.href}>
+                  {link.text}
+                </a>
+              </li>
+            ))}
+          </ul>
+        </div>
+      ))}
+    </div>
+    <div className="divider my-4" />
+    {copyRight}
+    <div className="ml-auto mt-4">
+      <ul className="flex gap-4 ">
+        {socialLinks?.map(({ icon, href, text }) => (
+          <IconButton key={href}>{icon}</IconButton>
         ))}
-      </div>
-      <div className="divider my-4" />
-      {copyRight}
-      <div className="ml-auto">
-        <ul className="flex gap-4 ">
-          {socialLinks?.map(({ icon, href, text }) => (
-            <IconButton key={href}>{icon}</IconButton>
-          ))}
-        </ul>
-      </div>
-    </footer>
-  );
-};
+      </ul>
+    </div>
+  </footer>
+);
